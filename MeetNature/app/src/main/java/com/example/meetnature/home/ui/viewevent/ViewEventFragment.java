b2fe9696af0890d520bd91f33caf4dd4a56574a3
BataDare <dareministar98@gmail.com>
2021-07-03 12:13:19 +0200

    Veiw Event fragment layout added

---
+++ MeetNature/app/src/main/java/com/example/meetnature/home/ui/viewevent/ViewEventFragment.java
@@ -0,0 +1,38 @@
+package com.example.meetnature.home.ui.viewevent;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetnature.R;
+
+public class ViewEventFragment extends Fragment {
+
+    private ViewEventViewModel mViewModel;
+
+    public static ViewEventFragment newInstance() {
+        return new ViewEventFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.view_event_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ViewEventViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
