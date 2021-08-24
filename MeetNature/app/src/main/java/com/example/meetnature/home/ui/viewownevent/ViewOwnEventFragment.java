6dbaab62b45c4b6d97348b6830ee3d710efee7a3
BataDare <dareministar98@gmail.com>
2021-08-25 00:28:02 +0200

    View own event layout added

---
+++ MeetNature/app/src/main/java/com/example/meetnature/home/ui/viewownevent/ViewOwnEventFragment.java
@@ -0,0 +1,38 @@
+package com.example.meetnature.home.ui.viewownevent;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetnature.R;
+
+public class ViewOwnEventFragment extends Fragment {
+
+    private ViewOwnEventViewModel mViewModel;
+
+    public static ViewOwnEventFragment newInstance() {
+        return new ViewOwnEventFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.view_own_event_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ViewOwnEventViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ MeetNature/app/src/main/java/com/example/meetnature/home/ui/viewownevent/ViewOwnEventViewModel.java
@@ -0,0 +1,7 @@
+package com.example.meetnature.home.ui.viewownevent;
+
+import androidx.lifecycle.ViewModel;
+
+public class ViewOwnEventViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
